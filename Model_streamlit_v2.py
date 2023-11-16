506561fa13e2ba6117ba5f3347f9b80cd5042eda
Prode <69654968+Fedemazza@users.noreply.github.com>
2023-11-16 18:49:45 -0300

    Add files via upload

--- Model_streamlit_v2.py
+++ Model_streamlit_v2.py
@@ -49,9 +49,9 @@ df_search_bench = df_search_bench.set_index('Tipo Search')
 df_FB_bench = load_clients('fb_bench')
 df_FB_bench = df_FB_bench.set_index('Tipo FB')
 
-df_YT_bench = load_clients('YT_bench')
+df_YT_bench = load_clients('bench_yt')
 df_YT_bench = df_YT_bench.set_index('Tipo YT')
-
+st.write(df_YT_bench)
 
 
 
